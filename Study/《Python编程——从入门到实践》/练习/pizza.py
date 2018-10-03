4c931bd5ba9aac40182aea791fd8827aded05bd7
tommy-zhouketao <zhouketao@csu.edu.cn>
2018-10-03 20:33:23 +0800

    update

---
+++ Study/《Python编程——从入门到实践》/练习/pizza.py
@@ -0,0 +1,8 @@
+# coding=UTF-8
+def make_pizza(size, *toppings):
+    """概述要制作的披萨"""
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+def foo():
+    print 1
--- Study/《Python编程——从入门到实践》/练习/test.py
+++ Study/《Python编程——从入门到实践》/练习/test.py
@@ -1,4 +1,10 @@
-def display_message():
-    print "Hello World"
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
 
-display_message()
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super(ElectricCar, self).__init__(make, model, year)
